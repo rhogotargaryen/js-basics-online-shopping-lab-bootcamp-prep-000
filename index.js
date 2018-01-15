cbe92f537e276c393b3c000d0a14afb633dacaeb
Derek Haller <dhaller.personal@gmail.com>
2018-01-15 06:34:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,91 +10,73 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var i = cart.length
- var pushcart = {};
- pushcart[item] = Math.floor(Math.random() * 100 + 1);
- cart[i] = pushcart;
- console.log(`${item} has been added to your cart.`);
- return cart;
+  var nuCart = {};
+  nuCart[item] = Math.floor(Math.random() * 100);
+  cart.push(nuCart);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  var i = cart.length;
-  var ai = cart.length;
-  var atKeys = [];
-  var atValues= [];
-  var atObject= {};
-  for (; i > 0; --i) {
-    atKeys.unshift (Object.keys (cart[i-1]));
-    }
-    console.log (atKeys);
-  for (; ai > 0; --ai) {
-    atObject = cart[ai-1];
-    atValues.unshift (atObject[atKeys[ai-1]]);
-    }
-    console.log (atValues);
+  var nuValues = [];
+  var nuKeys = [];
+  var nuObj = {};
+  for (var i = 0; i < cart.length; i++) {
+    nuObj = cart[i];
+    nuKeys.push(Object.keys(nuObj))
+  }
+  for (i = 0; i < cart.length; i++) {
+    nuObj = cart[i];
+    nuValues[i] = nuObj[nuKeys[i]];
+  }
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.");
   }
-  else if (cart.length == 1) {
-    console.log(`In your cart, you have ${atKeys[0]} at $${atValues[0]}.`);
+  else if (cart.length === 1) {
+    console.log(`In your cart, you have ${nuKeys[0]} at $${nuValues[0]}.`);
   }
-  else if (cart.length == 2) {
-    var atStringArray = [];
-    var bi = cart.length;
-    for (; bi > 0; --bi) {
-      atStringArray.unshift(`${atKeys[bi-1]} at $${atValues[bi-1]}`);
-    }
-    atStringArray = atStringArray.join(' and ');
-    console.log(`In your cart, you have ${atStringArray}.`);
+  else if (cart.length === 2) {
+    console.log(`In your cart, you have ${nuKeys[0]} at $${nuValues[0]} and ${nuKeys[1]} at $${nuValues[1]}.`);
   }
-  else if (cart.length >= 3) {
-    var atStringArray2 = [];
-    var ci = cart.length;
-    for (; ci > 0; --ci) {
-      atStringArray2.unshift(`${atKeys[ci-1]} at $${atValues[ci-1]}`);
+  else {
+    var nuStatements = [];
+    for (i = 0; i < cart.length; i++) {
+      nuStatements[i] = `${nuKeys[i]} at $${nuValues[i]}`;
     }
-    var lastItem = atStringArray2.pop();
-    atStringArray2= atStringArray2.join(", ")
-    console.log(`In your cart, you have ${atStringArray2}, and ${lastItem}.`);
-
+    var nuLast = nuStatements.splice(-1);
+    console.log(`In your cart, you have ${nuStatements.join(", ")}, and ${nuLast}.`);
   }
 }
 
 function total() {
-  var totalValue = 0;
-  var i = cart.length;
-  for (; i > 0; --i) {
-    var itemPair = cart[i-1];
-    for (var x in itemPair);
-    var addPrice = itemPair[x];
-    var totalCart = totalValue + addPrice;
-      totalValue = totalCart;
-    }
-    return totalCart;
+  var nuObj = {};
+  var nuKeys = [];
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    nuObj = cart[i];
+    nuKeys.push(Object.keys(nuObj));
   }
+  for (i = 0; i < cart.length; i++) {
+    var addThis = cart[i][nuKeys[i]];
+    total += addThis;
+  }
+  return total;
+}
 
-  function removeFromCart(item) {
-    var i = cart.length;
-      for (; i > 0; --i) {
-      var objItem = cart[i-1];
-      var checkItem = Object.keys(objItem);
-      if (checkItem[0] == item) {
-        cart.splice(i-1, 1);
-        return cart;
-      }
-    }
-    if (i == 0) {
-      console.log ("That item is not in your cart.");
-    }
+function removeFromCart(item) {
+  var nuObj = {};
+  var nuKeys = [];
+  for (var i = 0; i < cart.length; i++) {
+    nuObj = cart[i];
+    nuKeys.push(Object.keys(nuObj));
   }
-function placeOrder(cardNumber) {
-  if (cardNumber == null ) {
-    console.log ("Sorry, we don't have a credit card on file for you.");
-  }
-  else {
-    var totalCart = total();
-    console.log (`Your total cost is $${totalCart}, which will be charged to the card ${cardNumber}.`);
-    cart = [];
+  for (i = 0; i < cart.length; i++) {
+    if (nuKeys[i] == item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+    else {
+      console.log(`That item is not in your cart.`)
+    }
   }
 }
