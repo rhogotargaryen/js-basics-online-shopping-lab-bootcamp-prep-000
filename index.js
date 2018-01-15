b457d9e78c0e59eb6e7b884771341d5ffc8e21ac
Derek Haller <dhaller.personal@gmail.com>
2018-01-15 06:35:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function removeFromCart(item) {
       return cart;
     }
     else {
-      return (`That item is not in your cart.`)
+      return `That item is not in your cart.`;
     }
   }
 }
